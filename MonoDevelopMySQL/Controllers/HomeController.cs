42173b58965f526a1fcfef7259d9b94688c8b355
celacomtech <celacomtech@gmail.com>
2016-09-13 18:07:11 -0400

    Display data from MySQL connection

--- MonoDevelopMySQL/Controllers/HomeController.cs
+++ MonoDevelopMySQL/Controllers/HomeController.cs
@@ -4,20 +4,31 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Mvc.Ajax;
+using MonoDevelopMySQL.ContextDbs;
+using MonoDevelopMySQL.Models;
 
 namespace MonoDevelopMySQL.Controllers
 {
 	public class HomeController : Controller
-	{
-		public ActionResult Index ()
+	{private PeopleContext db;
+
+		public HomeController()
 		{
+			this.db = new PeopleContext ();
+		}
+		public ActionResult Index()
+		{
+
 			var mvcName = typeof(Controller).Assembly.GetName ();
 			var isMono = Type.GetType ("Mono.Runtime") != null;
 
 			ViewData ["Version"] = mvcName.Version.Major + "." + mvcName.Version.Minor;
 			ViewData ["Runtime"] = isMono ? "Mono" : ".NET";
 
-			return View ();
+			List<Person> people;
+			people = this.db.People.ToList ();
+
+			return View (people);
 		}
 	}
 }
--- MonoDevelopMySQL/Models/Person.cs
+++ MonoDevelopMySQL/Models/Person.cs
@@ -1,12 +1,23 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
 
-namespace MonoDevelopMySQL
+namespace MonoDevelopMySQL.Models
 {
+	[Table("people")]
 	public class Person
 	{
+
 		public Person ()
 		{
 		}
+		[Column("id")]
+		public int Id {get;set;}
+		[Column("first_name")]
+		public string FirstName { get; set;}
+		[Column("last_name")]
+		public string LastName { get; set;}
+		[Column("birth_date")]
+		public DateTime? BirthDate { get; set;}
 	}
 }
 
